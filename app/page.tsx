b8c4830bccb5fe3c63303fe2314f04ca82911daf
PushkarChandra <pushkarchandra2005@gmail.com>
2026-02-26 18:50:10 +0530

    fixed footer github redirecting text link

--- app/page.tsx
+++ app/page.tsx
@@ -125,7 +125,7 @@ agent run
         <p>Built with ❤️ by Pushkar</p>
 
         <a
-          href="https://github.com/your-username/runo-agent"
+          href="hhttps://github.com/PushkargithubCSE/runo.git"
           target="_blank"
           className="inline-block mt-2 hover:text-white"
         >
